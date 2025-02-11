ec6a173e85bbd606c8eb5eb69157d05cdc595a40
Miguel Guia <guia.miguel.9@gmail.com>
2025-02-11 17:49:12 +0100

    test: main pages smokes

---
+++ tests/webpage.spec.ts
@@ -0,0 +1,48 @@
+import { ArticlesPage } from '../src/pages/articles.page';
+import { CommentsPage } from '../src/pages/comments.page';
+import { HomePage } from '../src/pages/home.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify service main pages', () => {
+  test('home page title', async ({ page }) => {
+    //arrange
+    const homePage = new HomePage(page);
+
+    // act
+    await homePage.goto();
+
+    //assert
+    const title = await homePage.title();
+    expect(title).toContain('GAD');
+  });
+
+  test('articles page title', async ({ page }) => {
+    //arrange
+    const articlesPage = new ArticlesPage(page);
+
+    // act
+    await articlesPage.goto();
+
+    //assert
+    const title = await articlesPage.title();
+    expect(title).toContain('Articles');
+  });
+
+  test('comments page title', async ({ page }) => {
+    //arrange
+    const commentsPage = new CommentsPage(page);
+
+    // act
+    await commentsPage.goto();
+
+    //assert
+    const title = await commentsPage.title();
+    expect(title).toContain('Comments');
+  });
+  test('home page title simple', async ({ page }) => {
+    //act
+    await page.goto('http://localhost:3000');
+    // assert
+    await expect(page).toHaveTitle(/GAD/);
+  });
+});
