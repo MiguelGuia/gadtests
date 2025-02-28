73c90215193c81c4870e7aa6c551a3deda27fcdf
Miguel Guia <guia.miguel.9@gmail.com>
2025-02-28 22:32:41 +0100

    test: login scenario

---
+++ tests/smoke/login.spec.ts
@@ -0,0 +1,26 @@
+import { LoginPage } from '../../src/pages/login.page';
+import { WelcomePage } from '../../src/pages/welcome.page';
+import test, { expect } from '@playwright/test';
+
+test.describe('Verify login', () => {
+  test(
+    'login with correct credentials',
+    { tag: '@GAD_R02_01' },
+    async ({ page }) => {
+      //arrange
+      const userEmail = 'Moses.Armstrong@Feest.ca';
+      const userPassword = 'test1';
+      const loginPage = new LoginPage(page);
+      await loginPage.goto();
+
+      //act
+
+      await loginPage.login(userEmail, userPassword);
+      const welcomePage = new WelcomePage(page);
+      const title = await welcomePage.title();
+
+      //assert
+      expect(title).toContain('Welcome');
+    },
+  );
+});
