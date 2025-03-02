e3b9916376fd603256046c8ca08a7d13d26654f8
Miguel Guia <guia.miguel.9@gmail.com>
2025-03-02 21:05:01 +0100

    test: negative for login

--- tests/smoke/login.spec.ts
+++ tests/smoke/login.spec.ts
@@ -4,20 +4,45 @@ import { testUser1 } from '../../src/test-data/user.data';
 import test, { expect } from '@playwright/test';
 
 test.describe('Verify login', () => {
-  test('login with correct credentials @GAD-R02-01', async ({ page }) => {
-    // Arrange
-    const userEmail = testUser1.userEmail;
-    const userPassword = 'test1';
-    const loginPage = new LoginPage(page);
+  test(
+    'login with correct credentials',
+    { tag: '@GAD-R02-01' },
+    async ({ page }) => {
+      // Arrange
+      const userEmail = testUser1.userEmail;
+      const userPassword = testUser1.userPassword;
+      const loginPage = new LoginPage(page);
 
-    // Act
-    await loginPage.goto();
-    await loginPage.login(userEmail, userPassword);
+      // Act
+      await loginPage.goto();
+      await loginPage.login(userEmail, userPassword);
 
-    const welcomePage = new WelcomePage(page);
-    const title = await welcomePage.title();
+      const welcomePage = new WelcomePage(page);
+      const title = await welcomePage.title();
 
-    // Assert
-    expect(title).toContain('Welcome');
-  });
+      // Assert
+      expect(title).toContain('Welcome');
+    },
+  );
+  test(
+    'reject login with incorrect password',
+    { tag: '@login rejection' },
+    async ({ page }) => {
+      //Arrange
+      const userEmail = testUser1.userEmail;
+      const userPassword = 'wrong-password';
+      const loginPage = new LoginPage(page);
+      const title = await loginPage.title();
+
+      //Act
+      await loginPage.goto();
+      await loginPage.login(userEmail, userPassword);
+
+      //Assert
+      await expect
+        .soft(loginPage.loginError)
+        .toHaveText('Invalid email or password');
+      expect.soft(title).toContain('Login');
+    },
+  );
 });
