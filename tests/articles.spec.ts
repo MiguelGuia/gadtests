bad2a6a09105c8b11491659d18b2b6dfba8dcbfc
Miguel Guia <guia.miguel.9@gmail.com>
2025-04-16 18:29:42 +0200

    test: user can access article

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -35,30 +35,38 @@ test.describe('Verify articles', () => {
       .soft(articlePage.articleBody)
       .toHaveText(articleData.body, { useInnerText: true });
   });
-  test('reject creating article without title, {@GAD-R04-01}', async () => {
-    //arrange
-    const articleData = randomNewArticle();
-    const expectedErrorMessage = 'Article was not created';
-    articleData.title = '';
+  test(
+    'reject creating article without title',
+    { tag: '@GAD-R04-01' },
+    async () => {
+      //arrange
+      const articleData = randomNewArticle();
+      const expectedErrorMessage = 'Article was not created';
+      articleData.title = '';
 
-    // act
-    await addArticleView.createArticle(articleData);
+      // act
+      await addArticleView.createArticle(articleData);
 
-    //assert
-    await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
-  });
-  test('reject creating article without body, {@GAD-R04-02}', async () => {
-    //arrange
-    const articleData = randomNewArticle();
-    const expectedErrorMessage = 'Article was not created';
-    articleData.body = '';
+      //assert
+      await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
+    },
+  );
+  test(
+    'reject creating article without body',
+    { tag: '@GAD-R04-02' },
+    async () => {
+      //arrange
+      const articleData = randomNewArticle();
+      const expectedErrorMessage = 'Article was not created';
+      articleData.body = '';
 
-    //act
-    await addArticleView.createArticle(articleData);
+      //act
+      await addArticleView.createArticle(articleData);
 
-    //assert
-    await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
-  });
+      //assert
+      await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
+    },
+  );
   test('reject creating article with title exceeding 128 signs, {@GAD-R04-02}', async () => {
     //arrange
     const articleData = randomNewArticle(129);
@@ -70,4 +78,24 @@ test.describe('Verify articles', () => {
     //assert
     await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
   });
+  test(
+    'user can access single article',
+    { tag: '@GAD-R04-03' },
+    async ({ page }) => {
+      //arrange
+      const articlePage = new ArticlePage(page);
+      const articleData = randomNewArticle();
+      await addArticleView.createArticle(articleData);
+      await articlesPage.goto();
+
+      //act
+      await page.getByText(articleData.title).click();
+
+      //assert
+      await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
+      await expect
+        .soft(articlePage.articleBody)
+        .toHaveText(articleData.body, { useInnerText: true });
+    },
+  );
 });
