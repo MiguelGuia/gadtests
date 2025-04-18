c2b0071dd33f809c6a9d91822a199eb5b3ecc27c
Miguel Guia <guia.miguel.9@gmail.com>
2025-04-18 11:28:34 +0200

    test; e2e expanded by deleting own article

--- tests/end-to-end/article-lifecycle.spec.ts
+++ tests/end-to-end/article-lifecycle.spec.ts
@@ -8,7 +8,7 @@ import { AddArticleView } from '../../src/views/add-article.view';
 import test, { expect } from '@playwright/test';
 
 test.describe.configure({ mode: 'serial' });
-test.describe('Create and verify articles', () => {
+test.describe('Create,verify and delete article', () => {
   let loginPage: LoginPage;
   let articlesPage: ArticlesPage;
   let addArticleView: AddArticleView;
@@ -51,5 +51,19 @@ test.describe('Create and verify articles', () => {
     await expect
       .soft(articlePage.articleBody)
       .toHaveText(articleData.body, { useInnerText: true });
+  });
+  test('user can delete his own article', { tag: '@GAD-R04-04' }, async () => {
+    //arrange
+    await articlesPage.gotoArticle(articleData.title);
+    await articlePage.deleteArticle();
+    //act
+    await articlePage.deleteIcon.click();
+    //assert
+    await articlesPage.waitforPageToLoadUrl();
+    const title = await articlesPage.title();
+    expect(title).toContain('Articles');
+
+    await articlesPage.searchArticle(articleData.title);
+    await expect(articlesPage.noResultText).toHaveText('No data');
   });
 });
