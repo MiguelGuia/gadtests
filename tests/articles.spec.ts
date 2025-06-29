00e58ba1fe170ab630eb43ab6d3ba928195b637a
Miguel Guia <guia.miguel.9@gmail.com>
2025-06-29 20:53:26 +0200

    wip: session storage handling in projects

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -1,23 +1,17 @@
 import { prepareRandomArticle } from '../src/factories/article.factory';
 import { ArticlePage } from '../src/pages/article.page';
 import { ArticlesPage } from '../src/pages/articles.page';
-import { LoginPage } from '../src/pages/login.page';
-import { testUser1 } from '../src/test-data/user.data';
 import { AddArticleView } from '../src/views/add-article.view';
 import test, { expect } from '@playwright/test';
 
 test.describe('Verify articles', () => {
-  let loginPage: LoginPage;
   let articlesPage: ArticlesPage;
   let addArticleView: AddArticleView;
 
   test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
     articlesPage = new ArticlesPage(page);
     addArticleView = new AddArticleView(page);
 
-    await loginPage.goto();
-    await loginPage.login(testUser1);
     await articlesPage.goto();
     await articlesPage.addArticleButtonLogged.click();
   });
@@ -41,7 +35,7 @@ test.describe('Verify articles', () => {
   );
   test(
     'reject creating article without title',
-    { tag: '@GAD-R04-01' },
+    { tag: ['@GAD-R04-01', '@logged'] },
     async () => {
       //arrange
       const articleData = prepareRandomArticle();
@@ -57,7 +51,7 @@ test.describe('Verify articles', () => {
   );
   test(
     'reject creating article without body',
-    { tag: '@GAD-R04-02' },
+    { tag: ['@GAD-R04-02', '@logged'] },
     async () => {
       //arrange
       const articleData = prepareRandomArticle();
@@ -71,20 +65,24 @@ test.describe('Verify articles', () => {
       await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
     },
   );
-  test('reject creating article with title exceeding 128 signs, {@GAD-R04-02}', async () => {
-    //arrange
-    const articleData = prepareRandomArticle(129);
-    const expectedErrorMessage = 'Article was not created';
+  test(
+    'reject creating article with title exceeding 128 signs',
+    { tag: ['@GAD-R04-02', '@logged'] },
+    async () => {
+      //arrange
+      const articleData = prepareRandomArticle(129);
+      const expectedErrorMessage = 'Article was not created';
 
-    // act
-    await addArticleView.createArticle(articleData);
+      // act
+      await addArticleView.createArticle(articleData);
 
-    //assert
-    await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
-  });
+      //assert
+      await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
+    },
+  );
   test(
     'user can access single article',
-    { tag: '@GAD-R04-03' },
+    { tag: ['@GAD-R04-03', '@logged'] },
     async ({ page }) => {
       //arrange
       const articlePage = new ArticlePage(page);
