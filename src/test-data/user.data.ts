dfeaff2f11ca5d2a28250110a7e8697ba65bb401
Miguel Guia <guia.miguel.9@gmail.com>
2025-07-01 16:33:08 +0200

    chore: refactor config placement and naming

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,4 +1,4 @@
-import { USER_EMAIL } from '../env.config';
+import { USER_EMAIL } from '../../config/env.config';
 import { LoginUserModel } from '@_src/models/user.model';
 
 export const testUser1: LoginUserModel = {
