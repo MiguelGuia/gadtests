f703d89148d07e10e0dd2f234c57e2c500a46f5e
Miguel Guia <guia.miguel.9@gmail.com>
2025-07-04 19:23:25 +0200

    chore: use page object methods in menu component

--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,6 +1,5 @@
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
-import { HomePage } from '@_src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify menu main buttons', () => {
@@ -13,8 +12,7 @@ test.describe('Verify menu main buttons', () => {
 
     // Act
     await articlesPage.goto();
-    await articlesPage.mainMenu.commentsButton.click();
-    const commentsPage = new CommentsPage(page);
+    const commentsPage = await articlesPage.mainMenu.clickCommentsButton();
     const title = await commentsPage.getTitle();
 
     expect(title).toContain(expectedCommentsTitle);
@@ -30,8 +28,7 @@ test.describe('Verify menu main buttons', () => {
 
     // Act
     await commentsPage.goto();
-    await commentsPage.mainMenu.articlesButton.click();
-    const articlesPage = new ArticlesPage(page);
+    const articlesPage = await commentsPage.mainMenu.clickArticlesButton();
     const title = await articlesPage.getTitle();
 
     // Assert
@@ -47,8 +44,7 @@ test.describe('Verify menu main buttons', () => {
 
     // Act
     await articlesPage.goto();
-    await articlesPage.mainMenu.homePage.click();
-    const homePage = new HomePage(page);
+    const homePage = await articlesPage.mainMenu.clickHomePageLink();
     const title = await homePage.getTitle();
 
     // Assert
