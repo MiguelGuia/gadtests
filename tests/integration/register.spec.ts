6cfaf3f23029b0d3cc97f7713299fdd95214740d
Miguel Guia <guia.miguel.9@gmail.com>
2025-07-08 18:47:42 +0200

    chore: refactor register action to return po

--- tests/integration/register.spec.ts
+++ tests/integration/register.spec.ts
@@ -1,8 +1,6 @@
 import { prepareRandomUser } from '@_src/factories/user.factory';
 import { RegisterUserModel } from '@_src/models/user.model';
-import { LoginPage } from '@_src/pages/login.page';
 import { RegisterPage } from '@_src/pages/register.page';
-import { WelcomePage } from '@_src/pages/welcome.page';
 import test, { expect } from '@playwright/test';
 
 test.describe('Verify register', () => {
@@ -17,31 +15,30 @@ test.describe('Verify register', () => {
   test(
     'register with correct data and login',
     { tag: '@GAD-R03-01' },
-    async ({ page }) => {
+    async () => {
       // Arrange
-      const loginPage = new LoginPage(page);
-      const welcomePage = new WelcomePage(page);
-      const expectedWelcomeTitle = 'Login';
+
+      const expectedWelcomeTitle = 'Welcome';
       const expectedAlertPopupText = 'User created';
+      const expectedLoginTitle = 'Login';
 
       //act
-      await registerPage.register(registerUserData);
+      const loginPage = await registerPage.register(registerUserData);
 
       //assert
       await expect(registerPage.alertPopUp).toHaveText(expectedAlertPopupText);
       await loginPage.waitforPageToLoadUrl();
       const titleLogin = await loginPage.getTitle();
-      expect.soft(titleLogin).toContain(expectedWelcomeTitle);
+      expect.soft(titleLogin).toContain(expectedLoginTitle);
 
-      // assert
-      await loginPage.login({
+      // assert test login
+      const welcomePage = await loginPage.login({
         userEmail: registerUserData.userEmail,
         userPassword: registerUserData.userPassword,
       });
-
       const titleWelcome = await welcomePage.getTitle();
 
-      expect(titleWelcome).toContain('Welcome');
+      expect(titleWelcome).toContain(expectedWelcomeTitle);
     },
   );
   test(
